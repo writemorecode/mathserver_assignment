df043cd04fc6446e9c5fd20ab753ea2d99502688
writemorecode <gustav.ae.karlsson@gmail.com>
2023-10-02 18:04:15 +0200

    Cleaned up make_data.py

--- make_data.py
+++ make_data.py
@@ -1,10 +1,19 @@
 from sklearn.datasets import make_blobs
-
-X, y = make_blobs(n_samples = 3000, n_features=2, centers=[(-5,5),(5,5),(-5,-5)], random_state=42)
-
-with open("kmeans-data.txt", "w") as fh:
-    for sample in X:
-        x,y = sample
-        fh.write(f"{x}\t{y}\n")
+import random
 
 
+def main():
+    X, y = make_blobs(
+        n_samples=1500,
+        n_features=2,
+        centers=[(-5, 5), (5, 5), (-5, -5)],
+        random_state=random.randint(0, 100),
+    )
+    with open("kmeans-data.txt", "w") as fh:
+        for sample in X:
+            x, y = sample
+            fh.write(f"{x}\t{y}\n")
+
+
+if __name__ == "__main__":
+    main()
